c0a74e82331516ec5029c7c481d2a8e5e8de8c24
Rishma383 <64632844+Rishma383@users.noreply.github.com>
2021-08-26 22:02:24 +0530

    update node 26_08_2021

--- config/db.js
+++ config/db.js
@@ -1,23 +1,23 @@
 const express=require("express");
 const mongoose = require("mongoose");
-// try {
-//   mongoose.connect("mongodb+srv://softinn:Softinn765!@cluster0.8yo49.mongodb.net/softinndb?retryWrites=true&w=majority", {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useCreateIndex: true,
-//   });
-//   console.log("Database Connected Successfully");
-// } catch (err) {
-//   console.log("Database Not Connected");
-// }
+ try {
+   mongoose.connect("mongodb+srv://softinn:Softinn765!@cluster0.8yo49.mongodb.net/softinndb?retryWrites=true&w=majority", {
+     useNewUrlParser: true,
+     useUnifiedTopology: true,
+     useCreateIndex: true,
+   });
+   console.log("Database Connected Successfully");
+ } catch (err) {
+   console.log("Database Not Connected");
+ }
 
-try {
-  mongoose.connect("mongodb://localhost:27017/Softinn", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  });
-  console.log("Database Connected Successfully");
-} catch (err) {
-  console.log("Database Not Connected");
-}
+// try {
+  // mongoose.connect("mongodb://localhost:27017/Softinn", {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+    // useCreateIndex: true,
+  // });
+  // console.log("Database Connected Successfully");
+// } catch (err) {
+  // console.log("Database Not Connected");
+// }
--- controller/orders.js
+++ controller/orders.js
@@ -8,39 +8,43 @@ class Orders{
   async getOrdersInKitchen(req, res) {
     try {
         let allOrders = await orderModel.aggregate([
-            {
-                $lookup: {
-                  from: "ordersdetails",
-                  localField: "_id",
-                  foreignField: "orderId",
-                  as: "order_info",
-                },
-              },       
-              {
-                $unwind: {
-                  path: "$order_info",
-                  //preserveNullAndEmptyArrays: true
-                }
-              },
-              {
-                $lookup: {
-                  from: "tables",
-                  localField: "order_info.tableId",
-                  foreignField: "_id",
-                  as: "order_info.tables",
-                },
-              },
-              {
-                $unwind: "$order_info.tables"
-              },        
-              {
-                $project: {
-                 _id:1,customerId:1,totalAmount:1,
-                 orderId:1,tableId:1,menuItemId:1,menuitemName:1,finalItemPrice:1,quantity:1,
-                 tableName:1,tableCapacity:1,
-                  login_info:"$order_info",
-                }
-              }
+          {
+            $graphLookup:{
+              from: "ordersdetails",
+              startWith:"$_id",
+              connectFromField:"_id",
+              connectToField:"orderId",  
+              as: "order",
+            }
+          },
+          {
+            $unwind: {
+              path: "$order",
+            }
+          },          
+          {
+            $lookup: {
+              from: "tables",
+              localField: "order.tableId",
+              foreignField: "_id",
+              as: "ordertables",
+            },
+          },
+          {
+            $unwind: {
+              path: "$ordertables",
+            }
+          },
+          {
+            $group: {
+              _id: "$_id",              
+              order: { $push: "$order" },
+              ordertables: { $first: "$ordertables" },
+              customerId:{$first:"$customerId"},
+              totalAmount:{$first:"$totalAmount"}
+            }
+          },
+          
         ]);    
         console.log(allOrders);    
         res.json({ orders: allOrders });
